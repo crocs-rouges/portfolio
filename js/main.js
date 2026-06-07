9c16c1272a8f9fa35867fe19a5f53ee22a0e598d
romain chevalier <r.chevalier@student.isartdigital.com>
2026-06-07 18:15:44 +0200

    chore: setup vite and three.js environment

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
+import gsap from 'gsap';
+
+console.log('Logic Crypt initialized');
+console.log('Three.js version:', THREE.REVISION);
