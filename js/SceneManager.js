1327ee0c2724a124579f4aab7029b2b5eaffba4a
romain chevalier <r.chevalier@student.isartdigital.com>
2026-06-07 18:26:47 +0200

    fix(scene): set pixel ratio and add dispose method

--- js/SceneManager.js
+++ js/SceneManager.js
@@ -7,12 +7,15 @@ export class SceneManager {
     
     this.scene = new THREE.Scene();
     this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas, antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.camera.position.set(5, 5, 5);
     this.camera.lookAt(0, 0, 0);
     
+    this.resizeHandler = this.onResize.bind(this);
     this.onResize();
-    window.addEventListener('resize', () => this.onResize());
+    window.addEventListener('resize', this.resizeHandler);
   }
 
   onResize() {
@@ -23,5 +26,10 @@ export class SceneManager {
 
   update() {
     this.renderer.render(this.scene, this.camera);
+  }
+
+  dispose() {
+    window.removeEventListener('resize', this.resizeHandler);
+    this.renderer.dispose();
   }
 }
